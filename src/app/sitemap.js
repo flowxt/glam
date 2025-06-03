f54dd35d408ef012addeefe248e6d8df90c0f184
florian <florianbarjon@icloud.com>
2025-06-03 14:25:41 +0200

    sitemap

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -10,16 +10,10 @@ export default function sitemap() {
       priority: 1.0,
     },
     {
-      url: `${baseUrl}/maquillage`,
+      url: `${baseUrl}/galerie`,
       lastModified: new Date(),
       changeFrequency: "weekly",
-      priority: 0.9,
-    },
-    {
-      url: `${baseUrl}/coiffure`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.9,
+      priority: 0.8,
     },
     {
       url: `${baseUrl}/onglerie`,
@@ -28,28 +22,10 @@ export default function sitemap() {
       priority: 0.9,
     },
     {
-      url: `${baseUrl}/prestations`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/galerie`,
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.7,
-    },
-    {
       url: `${baseUrl}/contact`,
       lastModified: new Date(),
       changeFrequency: "monthly",
-      priority: 0.6,
-    },
-    {
-      url: `${baseUrl}/a-propos`,
-      lastModified: new Date(),
-      changeFrequency: "monthly",
-      priority: 0.5,
+      priority: 0.7,
     },
   ];
 
